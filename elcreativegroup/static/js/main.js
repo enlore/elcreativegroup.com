695a211141bd7a2fdadccccfe9d0ffa2808f228d
enlore <n.e.lorenson@gmail.com>
2013-09-18 19:46:35 -0500

    Face slider thing

--- elcreativegroup/static/js/main.js
+++ elcreativegroup/static/js/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function () {
+    var leftFace = $('#left-face')
+        ,rightFace = $('#right-face')
+        ,faceContainer = $('div.face-box')
+        ,containerWidth = faceContainer.width()
+        ,offset = faceContainer.offset()
 
+    console.log('width: ' + containerWidth)
+
+    faceContainer.mouseenter(function (e) {
+        $(this).mousemove(function(e) {
+            leftFace.css({ width: containerWidth - (e.pageX - offset.left) })
+            rightFace.css({ width: e.pageX - offset.left })
+        }) 
+    }).mouseleave(function () {
+        leftFace.css({'width': '50%'}) 
+        rightFace.css({'width': '50%'})
+    })
+})
