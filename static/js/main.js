0319597273647ac01b33ed5775994a5e31a1b222
enlore <n.e.lorenson@gmail.com>
2013-11-20 17:34:11 -0600

    Off canvas nav

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,12 @@ $(document).ready(function () {
 
     var $nav_button = $('#nav-button')
         , $navbar = $('#navbar')
+        , $left_panel = $('#left-panel')
+        , $right_panel = $('#right-panel')
 
     $nav_button.on('click', function () {
         $navbar.toggleClass('active')
+        $left_panel.toggleClass('active')
+        $right_panel.toggleClass('active')
     })
 })
