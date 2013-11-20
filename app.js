9849d1a19d1023b5daa248dc2d5bd0956ac3eca8
enlore <n.e.lorenson@gmail.com>
2013-11-19 23:25:45 -0600

    Wake up!

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express')
+    , app = express()
+    , less_middleware = require('less-middleware')
+    , path = require('path')
+
+app.set('view engine', 'jade')
+app.set('views', path.join(__dirname, 'views'))
+
+var less_opts = {
+    prefix  : 'css'
+    , src   : path.join(__dirname, 'static')
+    , paths : path.join(__dirname, 'static')
+}
+
+app.configure('development', function () {
+    app.locals.pretty = true
+    less_opts.compress = false
+    less_opts.debug = true
+})
+
+app.configure('production', function () {
+    app.locals.pretty = false
+    less_opts.compress = true
+    less_opts.debug = false
+})
+
+app.use(less_middleware(less_opts))
+app.use(express.static(path.join(__dirname, 'static')))
+
+app.get('/', function (req, resp) {
+    resp.render('index', {
+        title       : 'HEY WOAH'
+        , headline  : 'IT IS A SITE'
+    })
+})
+
+exports.start = function (port) {
+    app.listen(port, function () {
+        console.info('** Listening on localhost:%s', port)
+    })
+}
