65c21aa2247d4436e87c6a681f59c712557273de
enlore <n.e.lorenson@gmail.com>
2013-12-04 00:11:20 -0600

    Routes, formatting

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var express = require('express')
-    , app = express()
-    , less_middleware = require('less-middleware')
-    , path = require('path')
+var express             = require('express')
+    , app               = express()
+    , less_middleware   = require('less-middleware')
+    , path              = require('path')
+    , routes            = require('./routes')
 
 app.set('view engine', 'jade')
 app.set('views', path.join(__dirname, 'views'))
@@ -27,9 +28,8 @@ app.configure('production', function () {
 app.use(less_middleware(less_opts))
 app.use(express.static(path.join(__dirname, 'static')))
 
-app.get('/', function (req, resp) {
-    resp.render('index', {})
-})
+app.get('/', routes.index)
+app.get('/tarboosh', routes.tarboosh)
 
 app.get('/palettes', function (req, resp) {
     resp.render('palettes')
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+exports.index = function (res, resp) {
+    resp.render('index')
+}
+
+exports.tarboosh = function (res, resp) {
+    resp.render('tarboosh-clarksville')
+}
