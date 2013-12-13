4c83cb93a2562bcdd9cc77244197486ef97cbe42
enlore <n.e.lorenson@gmail.com>
2013-12-13 15:09:53 -0600

    Close nav on click

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function () {
     console.info('Ready!')
 
-    var $nav_button = $('#nav-button')
-        , $navbar = $('#navbar')
-        , $left_panel = $('#left-panel')
-        , $right_panel = $('#right-panel')
+    var $nav_button         = $('#nav-button')
+        , $navbar           = $('#navbar')
+        , $left_panel       = $('#left-panel')
+        , $right_panel      = $('#right-panel')
+        , $portfolio_anchor = $('#portfolio-anchor')
 
     $nav_button.on('click', function () {
+        $navbar.toggleClass('active')
+        $left_panel.toggleClass('active')
+        $right_panel.toggleClass('active')
+    })
+
+    $portfolio_anchor.on('click', function () {
         $navbar.toggleClass('active')
         $left_panel.toggleClass('active')
         $right_panel.toggleClass('active')
