64b159ed42c0ab2c02aaca44771d4f82cc967ef4
enlore <n.e.lorenson@gmail.com>
2014-12-11 23:04:23 -0600

    Loading gif and form val

--- static/js/main.js
+++ static/js/main.js
@@ -1,76 +1,109 @@
-$(document).ready(function (e) {
+;(function ($) {
+    $(document).ready(function (e) {
+        //skrollr.init({forceHeight: false})
 
-    //skrollr.init({forceHeight: false})
+        /*
+         * Navbar squeeze
+         * ****************************/
+        $window = $(window)
+        $navbar = $('.navbar-elcg')
+        $logo = $('.logo')
+        $window.on('scroll', function (e) {
+            console.log($(window).scrollTop())
 
-    /*
-     * Navbar squeeze
-     * ****************************/
-    $window = $(window)
-    $navbar = $('.navbar-elcg')
-    $logo = $('.logo')
-    $window.on('scroll', function (e) {
-        console.log($(window).scrollTop())
+            if ($window.scrollTop() >= 100) {
+                $navbar.addClass('active')
+                $logo.addClass('active')
+            } else if ($window.scrollTop() < 100) {
+                $navbar.removeClass('active')
+                $logo.removeClass('active')
+            }
+        })
 
-        if ($window.scrollTop() >= 100) {
-            $navbar.addClass('active')
-            $logo.addClass('active')
-        } else if ($window.scrollTop() < 100) {
-            $navbar.removeClass('active')
-            $logo.removeClass('active')
-        }
-    })
+        /*
+         * Project planner interactions
+         * ****************************/
+        function validateForm(e, form, fieldsMessages) {
+            var hasErrors = false
+            // should i jQuery this element? what happens if i jQuery an already jQueried element?
+            $form = form
 
-    /*
-     * Contact form validation
-     * ****************************/
-    $contactForm = $('#contact-form')
+            fieldsMessages.forEach(function (elem, index) {
+                $field = $form.find("[name=" + elem.name +"]")
 
-    $contactForm.delegate('input.form-control, textarea', 'focus', function (e) {
-        $contactForm.find('h3.success').text('Send Us A Message').removeClass('success')
-    })
+                if ($field.val() === "") {
+                    $field.siblings('label').text(elem.message)
+                    $field.parent('.form-group').addClass('has-error')
+                    hasErrors = true
+                } else {
+                    $field.parent('.form-group').removeClass('has-error')
+                    $field.siblings('label').text(elem.label)
+                }
+            })
 
-    $contactForm.on('submit', function (e) {
-        e.preventDefault()
-
-        var fieldsMessages = [
-            {name: 'human_name', message: 'What\'s your name?', label: 'Your name:'},
-            {name: 'phone', message: 'Can we have your number?', label: 'Your phone:'},
-            {name: 'email', message: 'Can we have your email?', label: 'Your email:'},
-            {name: 'message', message: 'What can we help you with?', label: 'What can we do for you?'}
-        ]
-
-        var hasErrors = false
-
-        for (var i = 0; i < fieldsMessages.length; i ++) {
-            var field = $contactForm.find('[name=' + fieldsMessages[i].name + ']')
-            if (field.val() === '') {
-                console.log(fieldsMessages[i].message)
-                $contactForm.find('[for='+ fieldsMessages[i].name +']').text(fieldsMessages[i].message)
-                field.parent('.form-group').addClass('has-error')
-                hasErrors = true
-            } else {
-                $contactForm.find('[for='+ fieldsMessages[i].name +']').text(fieldsMessages[i].label)
-                field.parent('.form-group').removeClass('has-error')
+            if (!hasErrors) {
+                e.target.submit()
             }
         }
 
-        if (!hasErrors) {
-            e.target.submit()
-        }
+        var $projectPlanner = $('#project-planner')
+
+        var budgetElems = [
+            "#budget-1",
+            "#budget-2",
+            "#budget-3",
+            "#budget-4"
+        ]
+
+        budgetElems.forEach(function (elem, index) {
+            $projectPlanner.delegate(elem, 'click', function (e) {
+                // clear the visual cue from the other, now inactive controls
+                budgetElems.forEach(function (elem, index) {
+                    $(elem).find(".control").removeClass("active")
+                    $(elem).find('label').removeClass("active")
+                })
+
+                // toggle the visual cue on the active control
+                $(elem).find('label').addClass("active")
+                $(elem).find('.control').addClass("active")
+
+                // and set its state to checked
+                $(elem).find('input[type=radio]').prop("checked", true)
+            })
+        })
+
+        var servicesElems = [
+            "#branding-control",
+            "#social-media-control",
+            "#print-design-control",
+            "#web-design-control"
+        ]
+
+
+        servicesElems.forEach(function (elem) {
+            $projectPlanner.delegate(elem, 'click', function (e) {
+                $(elem).find("label").toggleClass("active")
+                $(elem).find("span").toggleClass("active")
+                $(elem).find(".control").toggleClass("active")
+
+                var checkbox = $(elem).find("input[type=checkbox]")
+                checkbox.prop("checked", !checkbox.is(":checked"))
+
+                console.log(checkbox.is(":checked"))
+            })
+        })
+
+        $projectPlanner.on("submit", function (e) {
+            e.preventDefault()
+
+            var ppFieldsMessages = [
+                {name: 'human_name', message: 'What\'s your name?', label: 'Your name:'},
+                {name: 'phone', message: 'Can we have your number?', label: 'Your phone:'},
+                {name: 'email', message: 'Can we have your email?', label: 'Your email:'},
+                {name: '', message: "", label: ""},
+            ]
+
+            validateForm(e, $projectPlanner, ppFieldsMessages)
+        })
     })
-
-    /*
-     * Project planner interactions
-     * ****************************/
-
-    var $projectPlanner = $('#project-planner')
-
-    $projectPlanner.delegate('.budget label', 'click', function (e) {
-        console.log(e.target, " clicked")
-        $(e.target).find('span').toggleClass("active")
-    })
-
-    $projectPlanner.delegate('.budget label span', 'click', function (e) {
-        $(e.target).toggleClass('active')
-    })
-})
+}).call(this, jQuery)
